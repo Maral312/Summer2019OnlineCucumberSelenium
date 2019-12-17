c51c95b638510d9079dae4ffcd6784aa99cf5ce7
Maral312 <maral@nuriles.com>
2019-12-17 11:49:48 -0600

    added more steps

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -6,7 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features"
+        features = "src/test/resources/features", // path to features
+        glue = "com/vytrack/step_definitions",   // path to step definitions
+        dryRun = false  // by default
+        // dryRun = true  // it will not run your tests, it will check if every
+        //single test step has code implementation, which is step definition method
 )
 public class CucumberRunner {
 }
